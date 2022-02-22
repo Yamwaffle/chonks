f81c706066e62c8bf304c4552705bd60f23f8ffb
Wenyen333 <73871351+Wenyen333@users.noreply.github.com>
2022-02-22 10:25:51 -0800

    Add files via upload

--- Content/js/chonks.js
+++ Content/js/chonks.js
@@ -10,7 +10,23 @@ $(document).ready(function()
             $(".navbar-toggler").trigger("click");
         })
     });
-})
+});
+
+function reveal() {
+    var reveals = document.querySelectorAll(".chonks-card");
+
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 150;
+
+        if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+        }
+    }
+}
+  
+window.addEventListener("scroll", reveal);
 
 
 function openProfileModal(person, backColor) {
